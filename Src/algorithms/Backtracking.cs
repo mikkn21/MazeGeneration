867854e3ea62e673607d0457925593ecaa68f665
Baneboo <mikkel_10@hotmail.com>
2025-02-07 13:03:35 +0100

    Fixed filtering for neighbours - ALG works now

--- Src/algorithms/Backtracking.cs
+++ Src/algorithms/Backtracking.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using MazeGen.maze;
 namespace MazeGen.Algorithms{
     public class Backtracking{
@@ -18,7 +19,6 @@ namespace MazeGen.Algorithms{
         
     
         public Maze GenerateMaze(Maze maze){
-            Console.WriteLine("Generating maze using Backtracking algorithm");
             Maze mazeCopy = maze.Copy();
             BuildMaze(0, 0, mazeCopy);
             return mazeCopy;    
@@ -34,18 +34,18 @@ namespace MazeGen.Algorithms{
             // Get unvisited neighbour cells in random order (LINQ)
             List<(Wall, char)> neighbours = maze.GetNeighbours(cx, cy)
                 .Where( cell => (cell.Item1 & Wall.visited) != Wall.visited)
-                // .OrderBy (cell => Guid.NewGuid())
+                .OrderBy (cell => Guid.NewGuid())
                 .ToList();
 
-
+         
             foreach ((Wall,char) cell in neighbours) {
                 int nx = cx + DX[cell.Item2];
                 int ny = cy + DY[cell.Item2];
-                maze.RemoveWallBetween(cx, cy, nx, ny);
-                Console.WriteLine("Removing wall between ({0},{1}) and ({2},{3})", cx, cy, nx, ny);
-                BuildMaze(nx, ny, maze);
+                if (!maze.HasVisited(nx, ny)) { 
+                    maze.RemoveWallBetween(cx, cy, nx, ny);
+                    BuildMaze(nx, ny, maze);
+                }
             }
-
         }
 
 
