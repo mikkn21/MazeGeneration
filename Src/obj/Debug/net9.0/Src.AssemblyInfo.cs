18b307a7cda94436e47d236fddd5eac94acbce74
Baneboo <mikkel_10@hotmail.com>
2025-02-19 12:02:15 +0100

    Alg is working with all button func. Stop button added also. Unit test updated

--- Src/obj/Debug/net9.0/Src.AssemblyInfo.cs
+++ Src/obj/Debug/net9.0/Src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+492ca5a7fe9465011e0a9fbabfaa790b622854e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c90454e64720c55e33d7002a4115cdaf62f3f210")]
 [assembly: System.Reflection.AssemblyProductAttribute("Src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
